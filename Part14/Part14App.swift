0bbda8b9f0eaedf0baa942717cdaea379a30cb8d
kota-aso <kotaaso1212@gmail.com>
2024-03-26 22:22:09 +0900

    Initial Commit

---
+++ Part14/Part14App.swift
@@ -0,0 +1,17 @@
+//
+//  Part14App.swift
+//  Part14
+//
+//  Created by Kota Aso on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Part14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
